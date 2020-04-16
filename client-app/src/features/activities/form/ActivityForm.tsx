0ca8a3f96b668c7591f456c8590083fee3eb2cf9
baluji29 <baluji29@gmail.com>
2020-04-16 09:22:06 +0530

    End of Section 9

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, {useState, FormEvent, useContext, useEffect} from 'react'
-import { Segment, Form, Button } from 'semantic-ui-react'
+import { Segment, Form, Button, Grid } from 'semantic-ui-react'
 import { IActivity } from '../../../app/models/activity'
 import {v4 as uuid} from 'uuid';
 import ActivityStore from '../../../app/stores/activityStore'
@@ -64,7 +64,9 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match,histor
 
 
     return (
-        <Segment clearing>
+        <Grid>
+            <Grid.Column width={10}>
+            <Segment clearing>
             <Form onSubmit={handleSubmit}>
                 <Form.Input
                  onChange={handleInputChange} 
@@ -102,6 +104,9 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match,histor
                 <Button onClick={() => history.push('/activities')} floated = 'right' type='button' content='Cancel' />
             </Form>
         </Segment>
+            </Grid.Column>
+        </Grid>
+        
     )
 }
 
